bc14dbc0bd9853abfb8b322b2226f0a2d7ced373
Sree Aleti <root@repo.aletilabs.com>
2014-12-26 17:21:21 -0700

    Updated web file with require bareos server.

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,22 +1,22 @@
 class bareos::web (
-  $db_pass,
+  $db_pass = $bareos::params::default_pass,
   $db_user = $bareos::params::bareos_db_user,
   $db_name = $bareos::params::bareos_db_name,
   $db_host = $bareos::params::bareos_db_host,
   $webacula_config_temp = "bareos/webacula_config.ini.erb",
   $webacula_db_inst_temp = "bareos/webacula_db.conf.erb",
-  $webacula_timezone ='America/Denver',
   $web_pass = 'changeme',
 ) inherits bareos::params {
-    #include apache::params
+    require bareos::server
+	#include apache::params
     #include apache::service
 
     package { $bareos::params::webacula_pkgs: ensure => installed }
     #package { $apache::params::mod_packages['ssl']: ensure => latest, notify => Class['apache::service'] }
 
     file { "/var/www/webacula/application/config.ini":
-        owner   => root,
-        group   => root,
+        owner   => apache,
+        group   => apache,
         mode    => 644,
         content => template($webacula_config_temp),
         require => Package[$bareos::params::webacula_pkgs],
